267115916fe2907447b76170ae8408bc21a91e89
Lilian Etchepare <letchepare@2MORO.LAN>
2018-10-30 17:29:43 +0100

    Modifications de l'application de gestion de note en Ionic 3

--- Ionic/notes-app/src/pages/liste-notes/liste-notes.ts
+++ Ionic/notes-app/src/pages/liste-notes/liste-notes.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ActionSheetController, AlertController } from 'ionic-angular';
 import { RestProvider } from '../../providers/rest/rest';
 import { DetailNotePage } from '../detail-note/detail-note'
 
@@ -20,40 +20,118 @@ import { EditNotePage } from '../edit-note/edit-note';
   templateUrl: 'liste-notes.html',
 })
 export class ListeNotesPage {
-
+  recherche: string = '';
   notes: any;
-  pages: Array<{title: string, component: any}>;
+  notesDefault;
+  pages: Array<{ title: string, component: any }>;
   rootPage: any = HomePage;
 
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public restProvider: RestProvider) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public restProvider: RestProvider, public actionSheetCtrl: ActionSheetController, public alertCtrl: AlertController) {
     this.getNotes();
     this.pages = [
       { title: 'Home', component: HomePage },
       { title: 'List', component: ListPage },
-      { title: 'Notes', component: ListeNotesPage}
+      { title: 'Notes', component: ListeNotesPage }
     ];
+  }
+
+  presentActionSheet(note) {
+    const actionSheet = this.actionSheetCtrl.create({
+      title: 'Note N°' + note.id,
+      buttons: [
+        {
+          text: 'Lire la note',
+          icon: 'eye',
+          handler: () => {
+            this.lireNote(note);
+          }
+        }, {
+          text: 'Éditer la note',
+          icon: 'create',
+          handler: () => {
+            this.editerNote(note);
+          }
+        },
+        {
+          text: 'Supprimer la note',
+          role: 'destructive',
+          icon: 'trash',
+          handler: () => {
+            this.validerSuppression(note);
+          }
+        }, {
+          text: 'Annuler',
+          role: 'cancel',
+          icon: 'close'
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+
+  validerSuppression(note) {
+    const confirm = this.alertCtrl.create({
+      title: 'Suppression de la note N°' + note.id,
+      message: 'Êtes-vous certain de vouloir supprimer cette note?',
+      buttons: [
+        {
+          text: 'Non',
+        },
+        {
+          text: 'Oui',
+          handler: () => {
+            this.supprimerNote(note);
+          }
+        }
+      ]
+    });
+    confirm.present();
+ 
+  }
+
+  private supprimerNote(note: any) {
+    this.restProvider.supprimerNote(note).subscribe(() => this.getNotes());
+  }
+
+  private editerNote(note) {
+    this.navCtrl.push(EditNotePage, { 'note': note });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ListeNotesPage');
   }
 
-  ionViewWillEnter(){
+  ionViewWillEnter() {
     this.getNotes()
   }
 
-  getNotes(){
-    this.restProvider.getNotes().subscribe((res) => this.notes = res);
+  getNotes() {
+    this.restProvider.getNotes().subscribe((res) => this.notes = this.notesDefault = res);
   }
 
-  noteTapped(event,note){
-    this.navCtrl.push(DetailNotePage,{'note': note})
+  noteTapped(event, note) {
+    this.presentActionSheet(note)
   }
 
-  creerNote(event,note){
+  private lireNote(note) {
+    this.navCtrl.push(DetailNotePage, { 'note': note });
+  }
+
+  creerNote(event, note) {
     console.log("creer note")
     this.navCtrl.push(EditNotePage, { 'note': note });
+  }
+
+  getItems(event){
+    this.notes = this.notesDefault;
+    this.recherche = event.target.value;
+
+    if(this.recherche && this.recherche.trim() != ''){
+      this.notes = this.notes.filter((note) => {
+        return (note.titre.toLowerCase().indexOf(this.recherche.toLowerCase()) > -1)
+      })
+    }
   }
 
 }
